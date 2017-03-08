5cbc8c6a0ddfbd56c3f0169546e1d55007ac98a2
andy-ez <aezeanaka@gmail.com>
2017-03-08 21:10:21 +0000

    syntax

--- app/helpers/my_form_builder.rb
+++ app/helpers/my_form_builder.rb
@@ -2,7 +2,6 @@ class MyFormBuilder < ActionView::Helpers::FormBuilder
   def label(method, text = nil, options = {}, &block)
     errors = object.errors[method.to_sym]
     if errors
-      ...
     end
     super(method, text.html_safe, options, &block)
   end
