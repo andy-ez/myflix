c239c9075a29f4f18ada9d6248a4a5125f2ac8d4
andy-ez <aezeanaka@gmail.com>
2017-03-13 20:48:06 +0000

    advanced search features

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -11,6 +11,19 @@ class VideosController < ApplicationController
     @review = Review.new
   end
 
+  def advanced_search
+    options = {
+      reviews: params[:reviews],
+      rating_from: params[:rating_from],
+      rating_to: params[:rating_to]
+    }
+    if params[:query].present?
+      @videos = Video.search(params[:query], options).results
+    else
+      @videos = []
+    end
+  end
+
   def create_review
 
   end
--- app/models/video.rb
+++ app/models/video.rb
@@ -14,4 +14,54 @@ class Video < ActiveRecord::Base
     search.blank? ? all : where('title iLIKE ?', "%#{search}%").order(created_at: :asc)
   end
 
+  def average_rating
+    reviews.any? ? reviews.average(:rating).to_f.round(1) : 0.0
+  end
+  
+  def self.search(query, options={})
+    search_definition = {
+      query: {
+        bool: {
+          should: {
+            multi_match: {
+              query: query,
+              fields: ['title^100', 'description^50'],
+              operator: "and"
+            }
+          }
+        }
+      },
+      highlight: {
+        pre_tags: ["<em class='label label-highlight'>"],
+        post_tags: ["</em>"],
+        fields: {:"*" => {} }
+      }
+    }
+    if options[:reviews].present?
+      search_definition[:query][:bool][:should][:multi_match][:fields] << 'reviews.content'
+    end
+
+    if options[:rating_from].present? || options[:rating_to].present?
+      search_definition[:query][:bool][:must]= {
+        range: { 
+          average_rating: {
+            gte: (options[:rating_from] if options[:rating_from].present?),
+            lte: (options[:rating_to] if options[:rating_to].present?)
+          }
+        }
+      }
+    end
+    __elasticsearch__.search(search_definition)
+  end
+
+  def as_indexed_json(options={})
+    as_json(
+      methods: [:average_rating],
+      only: [:title, :description, :small_cover],
+      include: {
+        reviews: { only: [:content] }
+      }
+    )
+  end
+
 end
