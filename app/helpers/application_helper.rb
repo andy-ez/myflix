3c14fee3dbfc88142f019364cca0372033d104dc
andy-ez <aezeanaka@gmail.com>
2017-03-15 19:20:18 +0000

    view refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,20 @@ module ApplicationHelper
     review.rating ? "Rating: #{review.rating} / 5" : "Rating: None"
   end
 
-  def highlighted_text(video, field)
-    video.try(:highlight).try(field) ? video.highlight[field][0].html_safe : video.send(field)
+  def highlighted_text(video, field, display_field=nil)
+    if video.try(:highlight).try(field) 
+      video.highlight[field][0].html_safe 
+    else
+      !display_field ? video.try(field) : video.try(display_field)
+    end
+  end
+
+  def rating_options(name)
+    options_for_select((10..50).map { |num| num/10.0 }, params[name])
+  end
+
+  def select_tag_for_ratings(name)
+    select_tag name, rating_options(name), prompt: "-", class: "form-control"
   end
 
 end
